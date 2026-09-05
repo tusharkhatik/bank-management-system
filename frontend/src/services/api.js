4f9eb6529c39c77c1ec3710fa13744f912acc53d
Tushar Khatik <tusharkhatik347@gmail.com>
2026-09-05 14:07:20 +0530

    Configure frontend for Render backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:8080/api";
+const API_BASE_URL =  "https://bank-management-backend-5wh9.onrender.com/api";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
